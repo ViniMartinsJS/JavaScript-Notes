8a9639305748ddb4e92eab08466ef47f1b31c1fc
Vinicius Martins <viniciusmartinsjs1@gmail.com>
2023-06-23 15:14:52 -0300

    Primeiras funções do Front-end

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,36 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import HomeScreen from "./screens/home";
 import RegisterScreen from "./screens/auth/register";
 import LoginScreen from "./screens/auth/login";
 import NotesIndexScreen from "./screens/notes/index";
 import UserEditScreen from "./screens/users/edit";
 
-const RoutesApp = () => (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<HomeScreen />} />
-      <Route path="/register" element={<RegisterScreen />} />
-      <Route path="/login" element={<LoginScreen />} />
-      <Route path="/notes" element={<NotesIndexScreen />} />
-      <Route path="/users/edit" element={<UserEditScreen />} />
-    </Routes>
-  </BrowserRouter>
-);
+const PrivateRoute = ({ path, element: Element }) => {
+  const isUserAuthenticated = () => {
+    const user = localStorage.getItem("user");
+    return user !== null;
+  };
+
+  return (
+    <Route
+      path={path}
+      element={isUserAuthenticated() ? <Element /> : <Navigate to="/login" />}
+    />
+  );
+};
+
+const RoutesApp = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeScreen />} />
+        <Route path="/register" element={<RegisterScreen />} />
+        <Route path="/login" element={<LoginScreen />} />
+        <PrivateRoute path="/notes" element={NotesIndexScreen} />
+        <PrivateRoute path="/users/edit" element={UserEditScreen} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default RoutesApp;
